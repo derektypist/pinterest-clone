7f2311cef125b2a937e1bddaa8660d0f462cabb0
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-10-22 15:35:51 +0000

    Updated models.js to include Pin Schema

--- models.js
+++ models.js
@@ -7,7 +7,22 @@ const userSchema = new Schema({
   githubId: {type: String},
   password: {type: String},
   link: {type: String, required: true, unique: true},
-  image_links = [Schema.Types.ObjectId]
+  imagelinks: [Schema.Types.ObjectId]
 });
 
 const User = mongoose.model("pinterest-clone-users", userSchema);
+
+// Pin Schema
+const pinSchema = new Schema({
+  owner: {type: String, required: true},
+  ownerid: {type: Schema.Types.ObjectId, required: true},
+  ownerlink: {type: String, required: true},
+  link: {type: String, required: true, unique: true},
+  title: {type: String, required: true}
+});
+
+const Pin = mongoose.model("pinterest-clone-pins", pinSchema);
+
+// Export Modules
+module.exports.User = User;
+module.exports.Pin = Pin;
