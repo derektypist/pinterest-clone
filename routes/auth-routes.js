3243dacff730b0c08b05790251a406e196d5b166
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-10-25 10:39:57 +0000

    Added router sign in at auth-routes.js

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -4,3 +4,14 @@ const passport = require('passport');
 const flash = require('connect-flash');
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+router.use(flash);
+
+// Router Sign In
+router.get("/", (req,res) => {
+  if (req.user) {
+    res.redirect("/");
+  } else {
+    res.render('signin',req.flash('error'));
+  }
+});
