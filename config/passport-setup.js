c98240ea792b98c9d493cca367cf13756f152d04
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-10-28 11:33:27 +0000

    Added variables at passport-setup.js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,2 +1,8 @@
 // Set Up Variables
 const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const GithubStrategy = require('passport-github');
+const randombytes = require('randombytes');
+const User = require('../models').User;
+
+// Serialize and Deserialize 
