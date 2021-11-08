f743baae7061e0126ce5e129c556901e0dfcc8c3
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-11-08 16:17:49 +0000

    Updated helmet-setup.js

--- config/helmet-setup.js
+++ config/helmet-setup.js
@@ -1,4 +1,21 @@
 // Set Up Variable
-let helmetSetup = {};
+let helmetSetup = {
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'none'"],
+      styleSrc: ["'self'","https://fonts.googleapis.com","https://stackpath.bootstrapcdn.com"],
+      fontSrc: ["'self'","https://fonts.gstatic.com"],
+      imgSrc: ["'self'","https://via.placeholder.com","*"],
+      scriptSrc: ["'self'","https://code.jquery.com","https://stackpath.bootstrapcdn.com","https://unpkg.com"],
+      objectSrc: ["'none'"],
+      frameAncestors: ["'none'"],
+      baseUrl: ["'none'"],
+      formAction: ["'self'"],
+      upgradeInsecureRequests: true
+    }
+  },
+  hidePoweredBy: {setTo: "me"},
+  referrerPolicy: {policy:"strict-origin-when-cross-origin"}
+};
 
 module.exports = helmetSetup;
