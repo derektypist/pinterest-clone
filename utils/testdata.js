a879a05834835b113db7f7259b00e5c3c18b3220
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-11-11 16:05:00 +0000

    Added testdata.js

---
+++ utils/testdata.js
@@ -0,0 +1,8 @@
+// Create Test Data
+
+const mongoose = require("mongoose");
+const User = require("../models").User;
+const Pin = require("../models").Pin;
+const randombytes = require("randombytes");
+
+// Functions
