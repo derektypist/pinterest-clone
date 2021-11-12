ff83aef9c58a9eddc3dc0c02dbb006c474d91af0
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-11-12 17:44:18 +0000

    Added constants in server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+// Set Up Constants
+const express = require("express");
+const helmet = require("helmet");
+const passport = require("passport");
+require("./config/passport-setup");
+const mongoose = require("mongoose");
