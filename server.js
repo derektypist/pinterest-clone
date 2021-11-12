91be223acd17181846982a5dd6577843dcee072d
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-11-12 18:07:33 +0000

    Added template engine setup at server.js

--- server.js
+++ server.js
@@ -4,3 +4,17 @@ const helmet = require("helmet");
 const passport = require("passport");
 require("./config/passport-setup");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/auth-routes");
+const User = require("./models").User;
+const Pin = require("./models").Pin;
+const bodyParser = require("body-parser");
+const validator = require("validator");
+const app = express();
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+// Set Up Template Engine
+app.set('view engine','pug');
+app.set('views','./views');
+
+
